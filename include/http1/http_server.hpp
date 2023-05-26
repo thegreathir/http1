74422510edf56c536c10b28a011d4fd7a98670f4
thegreathir <aaph1377@gmail.com>
2023-05-26 14:53:11 +0330

    Add status code enum

--- include/http1/http_server.hpp
+++ include/http1/http_server.hpp
@@ -23,6 +23,79 @@ enum class HttpMethod {
   Patch
 };
 
+enum class HttpStatusCode {
+  // 1xx Informational
+  Continue = 100,
+  SwitchingProtocols = 101,
+  Processing = 102,
+
+  // 2xx Success
+  OK = 200,
+  Created = 201,
+  Accepted = 202,
+  NonAuthoritativeInformation = 203,
+  NoContent = 204,
+  ResetContent = 205,
+  PartialContent = 206,
+  MultiStatus = 207,
+  AlreadyReported = 208,
+  IMUsed = 226,
+
+  // 3xx Redirection
+  MultipleChoices = 300,
+  MovedPermanently = 301,
+  Found = 302,
+  SeeOther = 303,
+  NotModified = 304,
+  UseProxy = 305,
+  TemporaryRedirect = 307,
+  PermanentRedirect = 308,
+
+  // 4xx Client errors
+  BadRequest = 400,
+  Unauthorized = 401,
+  PaymentRequired = 402,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  NotAcceptable = 406,
+  ProxyAuthenticationRequired = 407,
+  RequestTimeout = 408,
+  Conflict = 409,
+  Gone = 410,
+  LengthRequired = 411,
+  PreconditionFailed = 412,
+  PayloadTooLarge = 413,
+  URITooLong = 414,
+  UnsupportedMediaType = 415,
+  RangeNotSatisfiable = 416,
+  ExpectationFailed = 417,
+  ImATeapot = 418,
+  MisdirectedRequest = 421,
+  UnprocessableEntity = 422,
+  Locked = 423,
+  FailedDependency = 424,
+  TooEarly = 425,
+  UpgradeRequired = 426,
+  PreconditionRequired = 428,
+  TooManyRequests = 429,
+  RequestHeaderFieldsTooLarge = 431,
+  UnavailableForLegalReasons = 451,
+
+  // 5xx Server errors
+  InternalServerError = 500,
+  NotImplemented = 501,
+  BadGateway = 502,
+  ServiceUnavailable = 503,
+  GatewayTimeout = 504,
+  HTTPVersionNotSupported = 505,
+  VariantAlsoNegotiates = 506,
+  InsufficientStorage = 507,
+  LoopDetected = 508,
+  NotExtended = 510,
+  NetworkAuthenticationRequired = 511
+};
+
 class HttpParseError : std::invalid_argument {
  public:
   HttpParseError(const std::string& error_message);
@@ -38,18 +111,18 @@ struct HeaderField {
   std::string value;
 
   static HeaderField Parse(const std::string& data);
-
 };
 
 using HeaderFields = std::vector<HeaderField>;
 
 class HttpRequest {
   friend std::ostream& operator<<(std::ostream& os, const HttpRequest& request);
+
  public:
   static HttpRequest Parse(const TcpServer::ByteArrayView& data);
   HttpRequest(HttpMethod method, const std::string& path,
               const std::string& version);
-  
+
   void add_field(const HeaderField& field);
   void set_body(const TcpServer::ByteArray& body);
 
