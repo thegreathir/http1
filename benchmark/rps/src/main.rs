aa29b0311c064db24e347abf03cf19ecc70431e9
thegreathir <aaph1377@gmail.com>
2023-05-27 21:24:06 +0330

    Fix hard coded options

--- benchmark/rps/src/main.rs
+++ benchmark/rps/src/main.rs
@@ -1,25 +1,26 @@
 use reqwest::blocking::Client;
-use std::{thread};
-use std::time::{Instant};
+use std::thread;
+use std::time::Instant;
 
 fn main() {
-    let n: i32 = std::env::args().nth(1).unwrap().parse().unwrap();
+    let number_of_threads: i32 = std::env::args().nth(1).unwrap().parse().unwrap();
+    let req_count: i32 = std::env::args().nth(2).unwrap().parse().unwrap();
+    let host: String = std::env::args().nth(3).unwrap().parse().unwrap();
     thread::scope(|s| {
         let mut join_handles = Vec::new();
-        const REQ_COUNT:i32 = 500000;
-        for _t in 0..n {
-            join_handles.push(s.spawn(|| {
+        for _t in 0..number_of_threads {
+            let host = host.clone();
+            join_handles.push(s.spawn(move || {
                 let client = Client::new();
                 let start = Instant::now();
-                for _i in 0..(REQ_COUNT / n) {
-                    let resp = client.get("http://127.0.0.1:8000").send().unwrap();
+                for _i in 0..(req_count / number_of_threads) {
+                    let resp = client.get(host.as_str()).send().unwrap();
                     if resp.status() != reqwest::StatusCode::OK {
                         println!("Bad status code");
                         break;
                     }
                 }
-                let duration = start.elapsed();
-                duration
+                start.elapsed()
             }));
         }
 
@@ -28,6 +29,6 @@ fn main() {
             .map(|x| x.join().unwrap())
             .fold(0., |acc, x| acc.max(x.as_secs_f64()));
 
-        println!("{}", f64::from(REQ_COUNT) / max_duration);
+        println!("{}", f64::from(req_count) / max_duration);
     });
 }
